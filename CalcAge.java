a6539239bbc027f13a296653636e5cf3bca5c2c6
ichinosee <67300586+ichinosee@users.noreply.github.com>
2020-09-02 20:42:50 +0900

    Add files via upload

---
+++ CalcAge.java
@@ -0,0 +1,26 @@
+package la.mgr;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class CalcAge {
+	public int howOld(int year, int month, int date) {
+		//誕生日を設定
+		Calendar cal = Calendar.getInstance();
+		cal.set(Calendar.YEAR, year);
+		cal.set(Calendar.MONTH, month-1);
+		cal.set(Calendar.DAY_OF_MONTH, date);
+
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
+
+		//現在時刻と誕生日を文字列にしてintする
+		int now = Integer.parseInt(sdf.format(new Date()));
+		int birth = Integer.parseInt(sdf.format(cal.getTime()));
+
+		//年齢を計算
+		//(20080811 - 19780220) / 1000
+		return (int)((now - birth) / 10000);
+	}
+
+}
